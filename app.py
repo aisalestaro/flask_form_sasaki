35986e3feb9513ef247412582305bbb46f8a8731
sales_taro <zituyuu777@gmail.com>
2023-10-07 14:22:09 +0900

    リファクタリング済

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "GET":
+@app.route("/", methods=["GET"])
+def index_get():
         return render_template("index.html")
-    else:
+
+
+@app.route("/", methods=["POST"])
+def index_post():
         message = request.form["message"]
         return render_template("index.html", message=message)
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
